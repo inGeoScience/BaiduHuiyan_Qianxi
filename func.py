ef7b6e04bc66e0ce2aff6aca5582ee7ea2c3a8ef
inGeoscience <cake@sunnyra.in>
2022-03-23 08:36:34 +0800

    fix

--- func.py
+++ func.py
@@ -7,159 +7,169 @@ import json
 import jsonpath
 
 
-def create_xlsx(json_path, date, scale_choice):
-    json_object = json.load(open(json_path, encoding="utf-8"))
-    scale_choice = int(scale_choice)
-    if scale_choice == 1:
-        province_list = jsonpath.jsonpath(json_object, "$.data.list[*].province_name")
-        value_list = jsonpath.jsonpath(json_object, "$.data.list[*].value")
-        if province_list and value_list:
-            df = pandas.DataFrame({
-                "Province_Name": province_list,
-                "Proportion": value_list
-            })
-            df.to_excel("./%s/%s.xlsx" % (json_path.split("/")[1], date))
-            print("%s.xlsx写入完成" % date)
+class getBaiduQianxi:
+    is_the_first_time = True
+    global_area_code = ''
+    def create_xlsx(self, json_path, date, scale_choice):
+        json_object = json.load(open(json_path, encoding="utf-8"))
+        scale_choice = int(scale_choice)
+        if scale_choice == 1:
+            province_list = jsonpath.jsonpath(json_object, "$.data.list[*].province_name")
+            value_list = jsonpath.jsonpath(json_object, "$.data.list[*].value")
+            if province_list and value_list:
+                df = pandas.DataFrame({
+                    "Province_Name": province_list,
+                    "Proportion": value_list
+                })
+                df.to_excel("./%s/%s.xlsx" % (json_path.split("/")[1], date))
+                print("%s.xlsx写入完成" % date)
+            else:
+                print("%s.xlsx写入失败，未读取到json数据" % date)
+
+        elif scale_choice == 2:
+            province_list = jsonpath.jsonpath(json_object, "$.data.list[*].province_name")
+            city_list = jsonpath.jsonpath(json_object, "$.data.list[*].city_name")
+            value_list = jsonpath.jsonpath(json_object, "$.data.list[*].value")
+            if province_list and city_list and value_list:
+                df = pandas.DataFrame({
+                    "Province_Name": province_list,
+                    "City_Name": city_list,
+                    "Proportion": value_list
+                })
+                df.to_excel("./%s/%s.xlsx" % (json_path.split("/")[1], date))
+                print("%s.xlsx写入完成" % date)
+            else:
+                print("%s.xlsx写入失败，json数据内没有内容或未获取json数据" % date)
+
+
+    def process_scale_choice(self, scale_choice):
+        base_url = ""
+        scale_choice = int(scale_choice)
+        if scale_choice == 1:
+            base_url = "https://huiyan.baidu.com/migration/provincerank.jsonp?"
         else:
-            print("%s.xlsx写入失败，未读取到json数据" % date)
+            base_url = "https://huiyan.baidu.com/migration/cityrank.jsonp?"
+        return base_url
 
-    elif scale_choice == 2:
-        province_list = jsonpath.jsonpath(json_object, "$.data.list[*].province_name")
-        city_list = jsonpath.jsonpath(json_object, "$.data.list[*].city_name")
-        value_list = jsonpath.jsonpath(json_object, "$.data.list[*].value")
-        if province_list and city_list and value_list:
-            df = pandas.DataFrame({
-                "Province_Name": province_list,
-                "City_Name": city_list,
-                "Proportion": value_list
-            })
-            df.to_excel("./%s/%s.xlsx" % (json_path.split("/")[1], date))
-            print("%s.xlsx写入完成" % date)
+
+    def process_date_choice(self, date_choice):
+        tiny_list = []
+        if int(date_choice) == 1:
+            tiny_list = ["20220110", "20220225"]
+            # tiny_list = ["20220110", time.strftime("%Y%m%d", time.localtime(time.time()))]
+        elif int(date_choice) == 2:
+            tiny_list = ["20210913", "20220109"]
+        elif int(date_choice) == 3:
+            tiny_list = ["20210119", "20210308"]
+        elif int(date_choice) == 4:
+            tiny_list = ["20200922", "20210118"]
+        elif int(date_choice) == 5:
+            tiny_list = ["20200110", "20200315"]
+        elif int(date_choice) == 6:
+            tiny_list.append(input("请输入起始日期，格式：20210101"))
+            tiny_list.append(input("请输入终止日期，格式：20210101"))
+        return tiny_list
+
+
+    def process_direction_choice(self, direction_choice):
+        direction_choice = int(direction_choice)
+        direction_str = ""
+        if direction_choice == 1:
+            direction_str = "move_in"
+        elif direction_choice == 2:
+            direction_str = "move_out"
+        return direction_str
+
+
+    def process_area_choice(self, area_choice):
+        area_choice = int(area_choice)
+        area_code = ""
+        if area_choice == 1:
+            area_code = "510100"
+        elif area_choice == 2:
+            area_code = "460100"
+        elif area_choice == 3:
+            area_code = input('请输入区域代码：')
+        return area_code
+
+
+    def generate_date_list(self, date_se_list:list):
+        pandas_date_list = pandas.date_range(date_se_list[0], date_se_list[1], freq="1D")
+        date_list = []
+        for date in pandas_date_list:
+            date_list.append(date.strftime("%Y%m%d"))
+        return date_list
+
+
+    def create_directory(self, area_choice, date_choice, direction_choice, scale_choice):
+        area_choice = int(area_choice)
+        date_choice = int(date_choice)
+        direction_choice = int(direction_choice)
+        scale_choice = int(scale_choice)
+        area = ""
+        date_range = ""
+        direction = ""
+        scale = ""
+        if area_choice == 1:
+            area = "成都"
+        elif area_choice == 2:
+            area = "海口"
+        elif area_choice == 3:
+            area = '自定义'
+
+        if date_choice == 1:
+            date_range = "2022春运"
+        elif date_choice == 2:
+            date_range = "2021国庆"
+        elif date_choice == 3:
+            date_range = "2021春运"
+        elif date_choice == 4:
+            date_range = "2020国庆"
+        elif date_choice == 5:
+            date_range = "2020春运"
+        elif date_choice == 6:
+            date_range = "自定义"
+
+        if direction_choice == 1:
+            direction = "迁入来源地"
+        elif direction_choice == 2:
+            direction = "迁出目的地"
+
+        if scale_choice == 1:
+            scale = "省级"
+        elif scale_choice == 2:
+            scale = "市级"
+
+        dir_name = "%s_%s_%s_%s" % (area, date_range, scale, direction)
+        dir_exists = os.path.exists(dir_name)
+        if dir_exists:
+            print("\"" + dir_name + "\"" + "目录已存在，将覆盖目录里同名文件")
         else:
-            print("%s.xlsx写入失败，json数据内没有内容或未获取json数据" % date)
+            os.mkdir(dir_name)
+
+        return dir_name
 
 
-def process_scale_choice(scale_choice):
-    base_url = ""
-    scale_choice = int(scale_choice)
-    if scale_choice == 1:
-        base_url = "https://huiyan.baidu.com/migration/provincerank.jsonp?"
-    else:
-        base_url = "https://huiyan.baidu.com/migration/cityrank.jsonp?"
-    return base_url
-
-
-def process_date_choice(date_choice):
-    tiny_list = []
-    if int(date_choice) == 1:
-        tiny_list = ["20220110", "20220225"]
-        # tiny_list = ["20220110", time.strftime("%Y%m%d", time.localtime(time.time()))]
-    elif int(date_choice) == 2:
-        tiny_list = ["20210913", "20220109"]
-    elif int(date_choice) == 3:
-        tiny_list = ["20210119", "20210308"]
-    elif int(date_choice) == 4:
-        tiny_list = ["20200922", "20210118"]
-    elif int(date_choice) == 5:
-        tiny_list = ["20200110", "20200315"]
-    elif int(date_choice) == 6:
-        tiny_list.append(input("请输入起始日期，格式：20210101"))
-        tiny_list.append(input("请输入终止日期，格式：20210101"))
-    return tiny_list
-
-
-def process_direction_choice(direction_choice):
-    direction_choice = int(direction_choice)
-    direction_str = ""
-    if direction_choice == 1:
-        direction_str = "move_in"
-    elif direction_choice == 2:
-        direction_str = "move_out"
-    return direction_str
-
-
-def process_area_choice(area_choice):
-    area_choice = int(area_choice)
-    area_code = ""
-    if area_choice == 1:
-        area_code = "510100"
-    elif area_choice == 2:
-        area_code = "460100"
-    return area_code
-
-
-def generate_date_list(date_se_list:list):
-    pandas_date_list = pandas.date_range(date_se_list[0], date_se_list[1], freq="1D")
-    date_list = []
-    for date in pandas_date_list:
-        date_list.append(date.strftime("%Y%m%d"))
-    return date_list
-
-
-def create_directory(area_choice, date_choice, direction_choice, scale_choice):
-    area_choice = int(area_choice)
-    date_choice = int(date_choice)
-    direction_choice = int(direction_choice)
-    scale_choice = int(scale_choice)
-    area = ""
-    date_range = ""
-    direction = ""
-    scale = ""
-    if area_choice == 1:
-        area = "成都"
-    elif area_choice == 2:
-        area = "海口"
-
-    if date_choice == 1:
-        date_range = "2022春运"
-    elif date_choice == 2:
-        date_range = "2021国庆"
-    elif date_choice == 3:
-        date_range = "2021春运"
-    elif date_choice == 4:
-        date_range = "2020国庆"
-    elif date_choice == 5:
-        date_range = "2020春运"
-    elif date_choice == 6:
-        date_range = "自定义"
-
-    if direction_choice == 1:
-        direction = "迁入来源地"
-    elif direction_choice == 2:
-        direction = "迁出目的地"
-
-    if scale_choice == 1:
-        scale = "省级"
-    elif scale_choice == 2:
-        scale = "市级"
-
-    dir_name = "%s_%s_%s_%s" % (area, date_range, scale, direction)
-    dir_exists = os.path.exists(dir_name)
-    if dir_exists:
-        print("\"" + dir_name + "\"" + "目录已存在，将覆盖目录里同名文件")
-    else:
-        os.mkdir(dir_name)
-
-    return dir_name
-
-
-def init_request(scale_choice, date, direction_choice, area_choice, dt_scale_choice):
-    base_url = process_scale_choice(scale_choice=scale_choice)
-    direction = process_direction_choice(direction_choice)
-    area_code = process_area_choice(area_choice)
-    dt_scale = 'dt_scale'
-    if int(dt_scale_choice) == 1:
-        dt_scale = 'city'
-    else:
-        dt_scale = 'province'
-    para = {
-        # 如果主体为省，则需要改dt
-        "dt": dt_scale,
-        "id": area_code,
-        "type": direction,
-        "date": date
-    }
-    url = base_url + urllib.parse.urlencode(para)
-    my_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)   Chrome/97.0.4692.71 Safari/537.36"}
-    request = urllib.request.Request(url=url, headers=my_headers)
-    return request
+    def init_request(self, scale_choice, date, direction_choice, area_choice):
+        is_the_first_time = True
+        base_url = self.process_scale_choice(scale_choice=scale_choice)
+        direction = self.process_direction_choice(direction_choice)
+        if self.is_the_first_time:
+            self.global_area_code = self.process_area_choice(area_choice)
+            self.is_the_first_time = False
+        dt_scale = 'dt_scale'
+        if int(self.global_area_code[-3]) == 0:
+            dt_scale = 'province'
+        elif not int(self.global_area_code[-3]) == 0:
+            dt_scale = 'city'
+        para = {
+            # 如果主体为省，则需要改dt
+            "dt": dt_scale,
+            "id": self.global_area_code,
+            "type": direction,
+            "date": date
+        }
+        url = base_url + urllib.parse.urlencode(para)
+        my_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)   Chrome/97.0.4692.71 Safari/537.36"}
+        request = urllib.request.Request(url=url, headers=my_headers)
+        return request
--- main.py
+++ main.py
@@ -4,27 +4,25 @@ import func
 import time
 
 
-dt_scale_choice = "id_scale_choice"
-while not dt_scale_choice.isnumeric():
-    dt_scale_choice = input("1.查询城市 2.查询省份")
+get_baidu_qianxi_data_func = func.getBaiduQianxi()
 area_choice = "area_choice"
 while not area_choice.isnumeric():
-    area_choice = input("1.成都 2.海口")
+    area_choice = input("1.成都 2.海口 3.自定义")
 scale_choice = "scale_choice"
 while not scale_choice.isnumeric():
-    scale_choice = input("1.省级 2.市级")
+    scale_choice = input("请输入迁入迁出排名单位\n1.省级 2.市级：")
 direction_choice = "direction_choice"
 while not direction_choice.isnumeric():
     direction_choice = input("1.迁入来源地 2.迁出目的地")
 date_choice = "date_choice"
 while not date_choice.isnumeric():
     date_choice = input("1.2022春运 2.2021国庆 3.2021春运 4.2020国庆 5.2020春运 6.自定义")
-base_url = func.process_scale_choice(scale_choice=scale_choice)
-date_se_list = func.process_date_choice(date_choice=date_choice)
-date_list = func.generate_date_list(date_se_list)
-dir_name = func.create_directory(area_choice, date_choice, direction_choice, scale_choice)
+base_url = get_baidu_qianxi_data_func.process_scale_choice(scale_choice=scale_choice)
+date_se_list = get_baidu_qianxi_data_func.process_date_choice(date_choice=date_choice)
+date_list = get_baidu_qianxi_data_func.generate_date_list(date_se_list)
+dir_name = get_baidu_qianxi_data_func.create_directory(area_choice, date_choice, direction_choice, scale_choice)
 for date in date_list:
-    request = func.init_request(scale_choice, date, direction_choice, area_choice, dt_scale_choice)
+    request = get_baidu_qianxi_data_func.init_request(scale_choice, date, direction_choice, area_choice)
     response = urllib.request.urlopen(request)
     content = response.read().decode("utf-8")
     content = content.split("(")[1].split(")")[0]
@@ -32,6 +30,6 @@ for date in date_list:
     file.write(content)
     file.close()
     print("%s.json写入完毕" % date)
-    func.create_xlsx("./%s/%s.json" % (dir_name, date), date, scale_choice)
+    get_baidu_qianxi_data_func.create_xlsx("./%s/%s.json" % (dir_name, date), date, scale_choice)
     time.sleep(5)
 print("结束")
